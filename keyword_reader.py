b8771ef9425221bfabbc8a2d769bc4a154daa71d
Linus Brewitz <linus.brewitz.5133@student.uu.se>
2026-04-13 14:28:12 +0200

    feat: added keyword reader

---
+++ keyword_reader.py
@@ -0,0 +1,19 @@
+from StatisticsExtractor import StatisticsExtractor
+import os
+import json 
+paths = "/home/linus-brewitz/Code/thesis/scyros/keywords"
+
+#NOTE: this is assuming there is only ONE keyword per file
+file_to_kw = dict()
+def read_json():
+    for e in os.scandir(paths):
+        if e.is_file():
+            with open(e.path, "r") as f:
+                data = json.load(f)
+                new_string = data["keywords"][0].replace("\\", "")
+                file_to_kw[e.name] = new_string
+    return file_to_kw
+    
+
+read_json()
+
