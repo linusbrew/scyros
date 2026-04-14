8ba002d27f2f842d347d331577ee626a77335eba
Linus Brewitz <linus.brewitz.5133@student.uu.se>
2026-04-14 09:18:45 +0200

    upd: init of dict now takes place in class

--- StatisticsExtractor.py
+++ StatisticsExtractor.py
@@ -1,16 +1,39 @@
 import polars as pl
+import os
+import json 
+
+paths = "/home/linus-brewitz/Code/thesis/scyros/keywords"
 
 class StatisticsExtractor:
     quantiles = [0.25, 0.50, 0.75]
+    file_to_kw = dict()
+
+    # NOTE: this is assuming there is only ONE keyword per file
+    def __init__(self):
+        for e in os.scandir(paths):
+            if e.is_file():
+                with open(e.path, "r") as f:
+                    data = json.load(f)
+                    new_string = data["keywords"][0].replace("\\", "")
+                    self.file_to_kw[e.name] = new_string
+
+    # Column can be either LOC or words, check how on avg how large a file is if
+    # it contains a keyword
+    #TODO: larger files will probably be more likely to contain keywords,
+    # so might need to use weighted somehow
+    def avg_length(self, df: pl.DataFrame, length: str, keyword: str) -> float:
+        foo = df.remove(pl.col(keyword) == 0)
+        bar = foo.select(pl.col(length).mean().round(2)).item()
+        return bar
 
     # NOTE: just in case I need to clean the data because of the errors
     def clean_projects(self, df: pl.DataFrame) -> pl.DataFrame:
         return df.remove(pl.col("path") == "error")
 
     # TODO: fix so the column does not have to be mentioned, i.e., it will just automatically
-    # work. to do this the program needs to know how many keywords I'm looking for
+    # work. to do this the program needs to know how and many and what keywords I'm looking for
     def kw_in_project(self, df: pl.DataFrame, column: str) -> int:
-        kw_count = df.select(pl.col(column) == 0).sum().item()
+        kw_count = df.select(pl.col(column)).sum().item()
         return kw_count
     
     # To see how the ratio between the total LOC and the LOC that contain 
@@ -18,10 +41,16 @@ class StatisticsExtractor:
     # be updated when doing this with multiple keywords
     # NOTE: this actually works for words as well since we specify
     # the columns before the call
-    def kw_ratio_project(self, df: pl.DataFrame, c1: str, c2: str) -> float:
-        return df.select((pl.col(c2).sum() / pl.col(c1).sum()) * 100).item()
+    def kw_ratio_project(self, df: pl.DataFrame, part: str, total: str) -> float:
+        return df.select(((pl.col(part).sum() / pl.col(total).sum()) * 100).round(2)).item()
 
-    #TODO: fix so I do not have so many seperate methods when they essentially
+    def max_keyword_project(self, df: pl.DataFrame, column: str) -> int:
+        return df.select(pl.col(column).max()).item()
+    
+    def min_keyword_project(self, df: pl.DataFrame, column: str) -> int:
+        return df.select(pl.col(column).min()).item()
+        
+    #TODO: fix so I do not have so many separate methods when they essentially
     # do the same thing. globals() might be useful
     def calculate_mean_all(self, df: pl.DataFrame) -> pl.DataFrame:
         return df.mean()
@@ -42,14 +71,14 @@ class StatisticsExtractor:
 
     def calculate_variance_attr(self, df: pl.DataFrame, column:str) -> int:
         df_var = df.var()
-        return df_var.select(pl.col(column))
+        return round(df_var.select(pl.col(column)).item(), 2)
 
     def calculate_sigma_all(self, df: pl.DataFrame) -> pl.DataFrame:
         return df.std()
 
     def calculate_sigma_attr(self, df: pl.DataFrame, column:str) -> int:
         df_sigma = df.std()
-        return df_sigma.select(pl.col(column)).item()
+        return round(df_sigma.select(pl.col(column)).item(), 2)
     
     def calculate_quant_all(self, df: pl.DataFrame) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
         result = list()
@@ -61,6 +90,6 @@ class StatisticsExtractor:
         result = list()
         foo = df.select(pl.col(column))
         for quantile in self.quantiles:
-            result.append(foo.quantile(quantile=quantile))
+            result.append(foo.quantile(quantile=quantile).item())
         return tuple(result)
     
