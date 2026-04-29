aef2bc70e1878c16f8c220670fab4c75635ac965
Linus Brewitz <linus.brewitz.5133@student.uu.se>
2026-04-29 11:20:28 +0200

    feat: added module-function calculations to stats file

--- python_stats.py
+++ python_stats.py
@@ -1,10 +1,12 @@
 import scripts.extract_stats_projects as extract_stats_projects
 import scripts.extract_stats_files as extract_stats_files
 import scripts.extract_stats_functions as extract_stats_functions
+import scripts.extract_module_function as extract_module_function
 
 if __name__ == "__main__":
     extract_stats_projects
     extract_stats_files
     extract_stats_functions
+    extract_module_function
     
 
