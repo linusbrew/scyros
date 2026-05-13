5df673a607b9192288ad77f7460f0eae229a8fc3
Linus Brewitz <linus.brewitz.5133@student.uu.se>
2026-05-13 15:34:41 +0200

    feat: updated so image_copy script also now copies stats over to the report

--- scripts/image_copy.sh
+++ scripts/image_copy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp -r --force /home/linus-brewitz/Code/thesis/scyros/figures/ /home/linus-brewitz/Documents/Thesis/thesis_report/
+
+cp -r --force /home/linus-brewitz/Code/thesis/scyros/stats/ /home/linus-brewitz/Documents/Thesis/thesis_report/
